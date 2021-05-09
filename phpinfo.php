5004b5ff367fc4c40c0c9e5ef96b655baeb0e004
Positive-Conative <kyj970513@naver.com>
2021-05-09 15:52:57 +0900

    Add error handling

--- phpinfo.php
+++ phpinfo.php
@@ -1,3 +1,7 @@
 <?php
 phpinfo();
+// C:\Bitnami\wampstack-8.0.5-0\php\php.ini >> developer
+// display_errors = On -> 에러 화면에 출력
+// log_errors = On ->로그 저장
+// C:\Bitnami\wampstack-8.0.5-0\apache2\logs\error.log
 ?>
