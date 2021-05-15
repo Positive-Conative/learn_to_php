97c4a5f9b9c905b4bec8ceaf814e17b56e505bce
Positive-Conative <kyj970513@naver.com>
2021-05-15 13:25:13 +0900

    숫자, 문자

---
+++ firstapp/helloworld.php
@@ -0,0 +1,15 @@
+<?php
+    var_dump(6);
+    echo '<br>';
+    var_dump("hello");
+    echo '<br>';
+    var_dump(1.23);
+    echo '<br>';
+    echo 1+1;       //숫자
+    echo '<br>';
+    echo "hello";   //문자
+?>
+
+<?php
+    echo "Hell World!"
+?>
