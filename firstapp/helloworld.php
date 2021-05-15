160ab369d0291fe3bdf5233fa1db8a24c1729e35
Positive-Conative <kyj970513@naver.com>
2021-05-15 17:59:05 +0900

    Post방식 확인

--- firstapp/helloworld.php
+++ firstapp/helloworld.php
@@ -7,10 +7,16 @@
     <title>Php Study</title>
 </head>
 <body>
-<?php
-    #http://localhost/firstapp/helloworld.php?id=conative&password=asdf
-    echo "환영합니다 ".$_GET['id']."님, 안녕하세용.";
-    echo "비밀번호는 ".$_GET['password']."네요.";
-?>
+    <form method="post" action="forms.php">
+        id: <input type="text" name="id"/><br>
+        pw: <input type="password" name="password"/> <br>
+        <input type="submit"/>
+    </form>
+
+    <?php
+        #http://localhost/firstapp/helloworld.php?id=conative&password=asdf
+        echo "환영합니다 ".$_GET['id']."님, 안녕하세용.";
+        echo "비밀번호는 ".$_GET['password']."네요.";
+    ?>
 </body>
 </html>
