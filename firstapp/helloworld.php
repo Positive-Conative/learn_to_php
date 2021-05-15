81fd3f3257c0d0092ccc2c1b723e439f66985783
Positive-Conative <kyj970513@naver.com>
2021-05-15 13:35:33 +0900

    변수 처리 1

--- firstapp/helloworld.php
+++ firstapp/helloworld.php
@@ -1,15 +1,23 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php Study</title>
+</head>
+<body>
 <?php
-    var_dump(6);
-    echo '<br>';
-    var_dump("hello");
-    echo '<br>';
-    var_dump(1.23);
-    echo '<br>';
-    echo 1+1;       //숫자
-    echo '<br>';
-    echo "hello";   //문자
+    // var_dump(1234);
+    // var_dump("1234");
+    // echo "hello"." "."world";
+    // echo 'i say "hello"';
+    
+    $a = 1;
+    echo $a + 1;
+    echo "<br />";  #주석! 
+    $a = 2;         /* 이것도 주석! */
+    print $a + 1;   // echo와 print는 같은 의미.
 ?>
-
-<?php
-    echo "Hell World!"
-?>
+</body>
+</html>
