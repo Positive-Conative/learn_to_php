5f67dacff967c2d463cd5239f23a39879d6378dc
Positive-Conative <kyj970513@naver.com>
2021-05-16 16:56:42 +0900

    Composer

--- firstapp/test.php
+++ firstapp/test.php
@@ -1,4 +1,9 @@
 <?php
-    //php.ini --> date.timezone = Asia/Seoul
-    echo date('Y-n-j H:m:s')
+    //Composer..! <의존성 관리>
+    require 'vendor/autoload.php';
+    use Michelf\MarkdownExtra;
+    $parser = new MarkdownExtra;
+    $parser->fn_id_prefix = "post22-";
+    $my_text = "#큰 글씨!";   //#은 h1, ##은 h2다.
+    echo $my_html = $parser->transform($my_text);
 ?>
