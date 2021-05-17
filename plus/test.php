29094bb884f33d871071587e85abc25fc882f7e4
Positive-Conative <kyj970513@naver.com>
2021-05-17 18:04:54 +0900

    Chapter 3 Checkpoint

--- plus/test.php
+++ plus/test.php
@@ -145,6 +145,65 @@
     // import_request_variables("gP", "rvar_");
     // print $rvar_foo
 ?>
+<?php
+    //IF문 동일.
 
+    //특이한 IF문
+    // $a = 4;
+    // if($a == 1): 
+    //     echo "1입니다!";
+    // elseif($a == 2):
+    //     echo "2입니다!";
+    // else:
+    //     echo "1,2 둘다 아닙니다!";
+    // endif;
+?>
+
+<?php
+    // break, continue는 뒤에 숫자를 지정해줌으로서 
+    // 어디서 빠져나갈지를 정할 수 있다.
+    // $i = 0;
+    // while(true){
+    //     echo "바깥반복!<br>";
+    //     while($i<5){
+    //         echo "안쪽반복!<br>";
+    //         if($i==3){
+    //             echo "끝!";
+    //             break 2;
+    //         }
+    //         $i++;
+    //     }
+    // }
+?>
+
+<?php
+    //for문도 if문처럼 특이한게 있다.
+
+    // for($i=0; $i<5; $i++):
+    //     echo($i);
+    // endfor;
+?>
+
+<?php
+    //에러코드를 뱉어내며 종료하기.
+    // $filename = '/path/to/test';
+
+    // exit = die >> 같음. 
+    // @$file = fopen($filename, 'r') or exit("파일이 없다!");
+    // @$file = fopen($filename, 'r') or die("파일이 없다!");   
+
+    //이후는 실행되지 않는다.
+    // echo "hello";
+    // exit(404);
+    // echo "world";
+?>
+
+<?php
+    //return --> includer.php에서 부름.
+    $b = 3 + 5;
+
+    return ($b);
+    echo "hello ";
+?>
 </body>
 </html>
